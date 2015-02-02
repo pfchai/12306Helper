2a4967c6926860cd8caf7a10cb6c92902f6bef5d
chai <861503125@qq.com>
2015-02-02 21:53:25 +0800

    finished TicketsData.py

--- TicketsData.py
+++ TicketsData.py
@@ -3,54 +3,30 @@
 # @author: cc <chai_pengfei@163.com>
 
 class TicketsData:
-    """ process tickets data"""
+    """ Beautiful print tickets data"""
 
-    def __init__(self):
-        self.seatType = {'硬座':'yz_num', '软座':'rz_num', '硬卧':'yw_num', '软卧':'rw_num', '一等座':'zy_num', '二等座':'ze_num',
-                 '无座':'wz_num', '其他':'qt_num', '商务座':'swz_num', '特等座':'gg_num', '高级软卧':'gr_num'}
-       #  self.seatType = {'No':'车次', 'st':'出发时间', 'at':'到达时间', 'la':'历时', seat = {'swz_num':'商务座', 'gg_num':'特等座',
-       #      'zy_num':'一等座','ze_num':'二等座', 'gr_num':'高级软卧', 'rz_num':'软卧', 'yw_num':'硬卧', 'rz_num':'软座',
-       #      'yz_num':'硬座', 'wz_num':'无座', 'qt_num':'其他'}}
-        self.tickets = []
-        self.beautifulFormat = "{0:^8}{1:^10} {2:^10} {3:^8}|{4:^10}{5:^10}{6:^10}{7:^10}{8:^10}{9:^8}{10:^8}{11:^8}{12:^8}{13:^8}{14:^8}"
-        # self.beautifulFormat = ("{No:^8}{st:^10} {at:^10} {la:^8}|"
-        #         "{seat[swz_num]:^10}{seat[gg_num]:^10}{seat[zy_num]:^10}{seat[ze_num]:^10}{seat[gr_num]:^10}{seat[rz_num]:^8}"
-        #         "{seat[yw_num]:^8}{seat[rz_num]:^8}{seat[yz_num]:^8}{seat[wz_num]:^8}{seat[qt_num]:^8}")
-        self.dataTitle = self.beautifulFormat.format('车次', '出发时间', '到达时间', '历时', '商务座', '特等座', '一等座',
-                '二等座', '高级软卧', '软卧', '硬卧', '软座', '硬座', '无座', '其他')
-        # self.beautifulData = self.dataTitle + '\n'
-        self.beautifulData = '\n'
+    def __init__(self, data):
+        self.data = data
+        self.beautifulFormat = u"{0:<9}{1:<9}{2:<9}{3:<9}|{4:^6}{5:^6}{6:^6}{7:^6}{8:^6}{9:^6}{10:^6}{11:^6}{12:^6}{13:^6}{14:^6}"
+        self.dataTitle = self.beautifulFormat.format(u'车次', u'出发时间', u'到达时间', u'历时', u'商务座', u'特等座', u'一等座',
+                u'二等座', u'高级软卧', u'软卧', u'硬卧', u'软座', u'硬座', u'无座', u'其他')
+        self.beautifulData = self.dataTitle
 
     def format_data(self, data):
-        for train in data:
-            print train
-            trainInfo = {}
-            trainInfo['trainNo'] = train['station_train_code']
-            trainInfo['trainStartTime'] = train['start_time']
-            trainInfo['trainArriveTime'] = train['arrive_time']
-            trainInfo['trainLast'] = train['lishi']
-            trainSeat = {}
-            for key, value in self.seatType.items():
-                trainSeat[key] = train[value]
-            trainInfo['trainSeat'] = trainSeat
-            self.tickets.append(trainInfo)
-        return self.tickets
+        pass
 
-    def beautifulData(self, tickets):
-        for train in tickets:
-            print train
+    def format_print(self):
+        for t in self.data:
+            self.beautifulData += u'\n' + self.beautifulFormat.format(t[u'station_train_code'], t[u'start_time'], t[u'arrive_time'], t[u'lishi'],
+                    t[u'swz_num'], t[u'gg_num'], t[u'zy_num'], t[u'ze_num'], t[u'gr_num'], t[u'rw_num'], t[u'yw_num'],
+                    t[u'rz_num'], t[u'yz_num'], t[u'wz_num'], t[u'qt_num'])
+        print self.beautifulData
 
-    def format_print(self, data):
-        tickets = self.format_data(data)
-        for t in tickets:
-            line = self.beautifulFormat.format(t['trainNo'], t['trainStartTime'], t['trainArriveTime'], t['trainLast'],
-                    t['trainSeat']['商务座'], t['trainSeat']['特等座'], t['trainSeat']['一等座'],
-                    t['trainSeat']['二等座'], t['trainSeat']['高级软卧'], t['trainSeat']['软卧'], t['trainSeat']['硬卧'],
-                    t['trainSeat']['软座'], t['trainSeat']['硬座'], t['trainSeat']['无座'], t['trainSeat']['其他'])
-            self.beautifulData += (line + '\n')
-
-    def __str__(self):
-        return self.beautifulData
 
 if __name__ == '__main__':
-    tb = TicketsData()
+    from Tickets import Tickets
+    t = Tickets('LFV', 'WXH', '2015-03-01', 'ADULT')
+    ticketsInfo = t.tickets_info()
+
+    tb = TicketsData(ticketsInfo)
+    tb.format_print()
